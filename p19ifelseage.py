c79b614b05743fc4570e0c666fd80ae65ca5143a
devprajapati07 <dp551797@gmail.com>
2025-01-08 14:36:57 +0530

    Add files via upload

---
+++ p19ifelseage.py
@@ -0,0 +1,20 @@
+age=int(input("Enter ur age =>"))
+
+if age>18:
+    print("u can vote")
+else:
+    print("Sorry u cant vote")
+
+"""
+# 1)Rs , 2000 , pizza - panipuri
+# 2)no1 no2 , greater
+# 3)mobile , sasta , costly
+# 4) 3 subjects , total , 50 , pass fail
+# 5) number , pos.neg
+# 6) number , odd/even
+# 7) year , leap year
+# 8) number1 , number2 , 1st div yes or no
+
+
+
+"""
